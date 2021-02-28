f6bbf1fc2119f160d7980dc8f858165dec3194c9
Arthur Ribeiro <arthurmr13@hotmail.com>
2021-02-28 11:34:38 -0300

    Finaliza agora tudo do stripe

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
     use HasFactory;
+
+    public function scopeMonthly(Builder $query)
+    {
+        return $query->where('api_id', getenv('MONTHLY_SUBSCRIPTION_ID'));
+    }
+
+    public function scopeQuarterly(Builder $query)
+    {
+        return $query->where('api_id', getenv('QUARTERLY_SUBSCRIPTION_ID'));
+    }
+
+    public function scopeSemiannual(Builder $query)
+    {
+        return $query->where('api_id', getenv('SEMIANNUAL_SUBSCRIPTION_id'));
+    }
 }
--- database/migrations/2021_02_28_134411_create_plans_table.php
+++ database/migrations/2021_02_28_134411_create_plans_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreatePlansTable extends Migration
@@ -15,10 +16,12 @@ class CreatePlansTable extends Migration
     {
         Schema::create('plans', function (Blueprint $table) {
             $table->id();
+            $table->string('api_id');
             $table->string('name');
-            $table->double('value');
             $table->timestamps();
         });
+
+        $this->setupData();
     }
 
     /**
@@ -29,5 +32,14 @@ class CreatePlansTable extends Migration
     public function down()
     {
         Schema::dropIfExists('plans');
+    }
+
+    private function setupData()
+    {
+        DB::table('plans')->insert([
+            ['id' => 1, 'api_id' => getenv('MONTHLY_SUBSCRIPTION_ID'), 'name' => 'Mensal'],
+            ['id' => 2, 'api_id' => getenv('QUARTERLY_SUBSCRIPTION_ID'), 'name' => 'Trimestral'],
+            ['id' => 3, 'api_id' => getenv('SEMIANNUAL_SUBSCRIPTION_id'), 'name' => 'Semestral'],
+        ]);
     }
 }
